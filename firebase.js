54356262780a6102552f0ba47933d4260780c7f6
Milo Bedini <milobedini64@gmail.com>
2022-01-10 23:28:36 +0000

    emulators

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB0aHn3gY2rg12RjtysZQ5Xqe0DF7W6S8U',
+  authDomain: 'parlay-10393.firebaseapp.com',
+  projectId: 'parlay-10393',
+  storageBucket: 'parlay-10393.appspot.com',
+  messagingSenderId: '775359686408',
+  appId: '1:775359686408:web:4fa726d76c5ba01244eebd',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
